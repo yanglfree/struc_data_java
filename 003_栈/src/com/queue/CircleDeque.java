18ecca1c7748a9f4ae526fcef0d36df662da68e0
YangLiang <yangliangsky@gmail.com>
2020-05-13 22:16:01 +0800

    clear方法完善

--- 003_栈/src/com/queue/CircleDeque.java
+++ 003_栈/src/com/queue/CircleDeque.java
@@ -3,10 +3,10 @@ package com.queue;
 public class CircleDeque<E> {
 	private int size;
 	private E[] elements;
-	private int front;//队头  头+size = 队尾
-	
+	private int front;// 队头 头+size = 队尾
+
 	private static final int DEFAUL_CAPACITY = 10;
-	
+
 	public CircleDeque() {
 		elements = (E[]) new Object[DEFAUL_CAPACITY];
 	}
@@ -14,26 +14,24 @@ public class CircleDeque<E> {
 	public int size() {
 		return size;
 	}
-	
+
 	public void enQueueFront(E element) {
 		ensureCapacity(size + 1);
 		front = index(-1);
 		elements[front] = element;
 		size++;
 	}
-	
+
 	public void enQueueRear(E element) {
 		ensureCapacity(size + 1);
 		elements[index(size)] = element;
 		size++;
 	}
-	
 
-	
 	public boolean isEmpty() {
 		return size == 0;
 	}
-	
+
 	public E deQueueFront() {
 		E frontElement = elements[front];
 		elements[front] = null;
@@ -41,7 +39,7 @@ public class CircleDeque<E> {
 		size--;
 		return frontElement;
 	}
-	
+
 	public E deQueueRear() {
 		int rearIndex = index(size - 1);
 		E rearElement = elements[rearIndex];
@@ -49,35 +47,37 @@ public class CircleDeque<E> {
 		size--;
 		return rearElement;
 	}
-	
+
 	private int index(int index) {
 //		index = front + index;
 //		if (index < 0) {
 //			return index + elements.length;
 //		}
 //		return index % elements.length;
-		
+
 		index = front + index;
 		if (index < 0) {
 			return index + elements.length;
 		}
 		return index - (index > elements.length ? elements.length : 0);
 	}
-	
+
 	public E front() {
 		return elements[front];
 	}
-	
+
 	public E rear() {
 		return elements[index(size - 1)];
 	}
-	
+
 	public void clear() {
-		for (int i = front; i < index(size); i++) {
-			elements[i] = null;
+		for (int i = front; i < size; i++) {
+			elements[index(i)] = null;
 		}
+		size = 0;
+		front = 0;
 	}
-	
+
 	/**
 	 * 确保容量够用，如果不够用，对数组进行1.5倍扩容
 	 * 
@@ -94,17 +94,18 @@ public class CircleDeque<E> {
 		E[] newElements = (E[]) new Object[newCapacity];
 
 		for (int i = 0; i < size; i++) {
-			newElements[i] = elements[index(i)]; //新数组从0开始重新赋值
+			newElements[i] = elements[index(i)]; // 新数组从0开始重新赋值
 		}
 		elements = newElements;
-		front = 0; //front 重置为0 ！！！！！
+		front = 0; // front 重置为0 ！！！！！
 		System.out.println(oldCapacity + "扩容为:" + newCapacity);
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder string = new StringBuilder();
-		string.append("capacity=").append(elements.length).append(", front=").append(front).append(", size=").append(size).append(", [");
+		string.append("capacity=").append(elements.length).append(", front=").append(front).append(", size=")
+				.append(size).append(", [");
 		for (int i = 0; i < elements.length; i++) {
 			if (i != 0) {
 				string.append(", ");
@@ -112,6 +113,6 @@ public class CircleDeque<E> {
 			string.append(elements[i]);
 		}
 		string.append("]");
-		return string.toString();	
+		return string.toString();
 	}
 }
